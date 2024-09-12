f62cd72da9537dcaf657a5e7490b343ddc8db016
Armaan Chandarana <achandarana@bayviewglen.ca>
2024-09-12 09:39:21 -0400

    Covered math, and printing

---
+++ Unit One - Introduction to Java/src/day1/ExampleThree.java
@@ -0,0 +1,23 @@
+package day1;
+
+public class ExampleThree {
+    public static void main(String[] args){
+        // int numStudents = 20;
+        // double volume = 0987.1234;
+        // boolean isClassLarge = numStudents > 20;
+
+        // System.out.println(numStudents);
+        // System.out.println(volume);
+        // System.out.println(isClassLarge);
+
+        int a = 5, b = 10;
+        System.out.println(a + b);
+
+        double c = 6.5, d = 10; // 10 is treated as 10.0
+        System.out.println(c + d);
+
+        System.out.println(c/d);
+        System.out.println(b/a); //outputs 1, cuts off the decimal
+        System.out.println(7%5);
+    }
+}
---
+++ Unit One - Introduction to Java/src/day1/ExampleTwo.java
@@ -0,0 +1,11 @@
+package day1; // comment with 2 slashes. Semi-colon at the end of each statment.
+
+public class ExampleTwo { //curly braces are used instead of indentation from python
+    public static void main(String[] args){
+        System.out.println("YAAAYYYYYYY! IT WORKSSSSSSSSSS! WOHOOOOOO");
+        System.out.println("sysout is faster than System.out.println");
+        System.out.println();
+        System.out.println("A");
+        System.out.println("B");
+    }
+}
