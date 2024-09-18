c5abcd6f719f000e16ba1592988b73058cc59470
Armaan Chandarana <achandarana@bayviewglen.ca>
2024-09-18 08:45:46 -0400

    homework

---
+++ Unit One - Introduction to Java/src/day2/Homework.java
@@ -0,0 +1,59 @@
+package day2;
+
+public class Homework {
+    public static void main(String[] args){
+        //QUESTION 1
+        // double price = 15.95;
+        // double total = price*3;
+
+        //QUESTION 2
+        // double length = 8.5;
+        // int width = 6;
+        // double area = length*width;
+
+        //QUESTION 3
+        // int s1 = 100;
+        // int s2 = 99;
+        // int s3 = 100;
+        // int s4 = 100;
+        // int s5 = 99;
+        // double average = (s1+s2+s3+s4+s5)/5.0;
+
+        //QUESTION 4
+        // int number = 137;
+        // int tenDigit = number/10%10;
+
+        //QUESTION 5
+        // int dividend = 10;
+        // int divisor = 3;
+        // int remainder = dividend%divisor;
+
+        //QUESTION 6
+        // final double pi = 3.1415;
+        // double r = 4.7;
+        // double c = 2*pi*r;
+
+        //QUESTION 7
+        // double price=12.50;
+        // int fee=15;
+        // double cost = (4*price)*(1 + fee/100.0);
+
+        //QUESTION 8
+        // int number = 456;
+        // int oneDigit = number%10;
+        
+        //QUESTION 9
+        // double base = 9.8;
+        // double height = 5.2;
+        // double area = (base*height)/2;
+        
+        //QUESTION 10
+        int dividend = 14;
+        int divisor = 7;
+        if (dividend%divisor==0){
+            System.out.println("True");
+        }else{
+            System.out.println("False");
+        }
+    }
+}
