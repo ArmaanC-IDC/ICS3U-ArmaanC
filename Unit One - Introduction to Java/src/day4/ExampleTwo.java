d463c37b1dae081f1775124b90c5acabbfb5ca61
Armaan Chandarana <achandarana@bayviewglen.ca>
2024-09-26 08:40:44 -0400

    finish unit one

---
+++ Unit One - Introduction to Java/src/day4/ExampleTwo.java
@@ -0,0 +1,16 @@
+package day4;
+
+public class ExampleTwo {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        
+        int x = 4;
+        int y = 7;
+
+        // int sum = x+y;
+        // System.out.println(sum);
+
+        System.out.println(x + " + " + y + " = " + (x+y));
+        // concatonation - joining strings
+    }
+}
